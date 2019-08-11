78e21d3582b2387e87dc176494574f280264c7fd
Vishal Singh <vishal19singh96@gmail.com>
2019-08-11 18:57:12 +0530

    black applied

--- toys/views.py
+++ toys/views.py
@@ -8,14 +8,15 @@ from rest_framework.decorators import api_view
 
 # Create your views here.
 
-@api_view(['GET', 'POST'])
+
+@api_view(["GET", "POST"])
 def toy_list(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         toys = Toy.objects.all()
         toys_serializer = ToySerializer(toys, many=True)
         return Response(toys_serializer.data)
 
-    elif request.method == 'POST':
+    elif request.method == "POST":
         toy_serializer = ToySerializer(data=request.data)
         if toy_serializer.is_valid():
             toy_serializer.save()
@@ -23,24 +24,24 @@ def toy_list(request):
         return Response(toy_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
+@api_view(["GET", "PUT", "DELETE"])
 def toy_detail(request, pk):
     try:
         toy = Toy.objects.get(pk=pk)
     except Toy.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+    if request.method == "GET":
         toy_serializer = ToySerializer(toy)
         return Response(toy_serializer.data)
 
-    elif request.method == 'PUT':
+    elif request.method == "PUT":
         toy_serializer = ToySerializer(toy, data=request.data)
         if toy_serializer.is_valid():
             toy_serializer.save()
             return Response(toy_serializer.data)
         return Response(toy_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    elif request.method == "DELETE":
         toy.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
